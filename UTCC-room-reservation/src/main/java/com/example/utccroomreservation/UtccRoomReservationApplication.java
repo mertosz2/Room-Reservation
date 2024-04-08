2bb5ab8da56e81fd2d83152e17eb0eb9c517f001
Jonathan Chen <sabza178@gmail.com>
2024-04-08 13:17:47 +0700

    Initializr project

---
+++ UTCC-room-reservation/src/main/java/com/example/utccroomreservation/UtccRoomReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.utccroomreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtccRoomReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtccRoomReservationApplication.class, args);
+    }
+
+}
